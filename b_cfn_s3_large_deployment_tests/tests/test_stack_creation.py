c2b6a7ccfeee6e27011d0dbb21566452387c4c7b
Matas Gumbinas <matas.gumbinas@biomapas.com>
2021-09-15 15:55:23 +0300

    Fix issue with missing imports in testing package and add two new files deployment tests.

--- b_cfn_s3_large_deployment_tests/tests/test_stack_creation.py
+++ b_cfn_s3_large_deployment_tests/tests/test_stack_creation.py
@@ -1,16 +1,18 @@
 import logging
 
 from b_aws_testing_framework.credentials import Credentials
+
 from b_cfn_s3_large_deployment_tests.infrastructure import Infrastructure
 
 logger = logging.getLogger(__name__)
 
 
-def test_stack_exists() -> None:
+def test_stack_created() -> None:
     """
     Tests that stack actually was created.
     :return: No return.
     """
+
     client = Credentials().boto_session.client('cloudformation')
 
     stacks = client.list_stacks(StackStatusFilter=['CREATE_COMPLETE'])['StackSummaries']
